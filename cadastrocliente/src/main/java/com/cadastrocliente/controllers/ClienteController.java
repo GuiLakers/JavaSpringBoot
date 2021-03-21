2e70a04317c8c0b569a324e53bcd8aad82cd5e0f
GuiLakers08 <guifun080808@gmail.com>
2021-03-21 20:27:55 +0000

    Up

--- cadastrocliente/src/main/java/com/cadastrocliente/controllers/ClienteController.java
+++ cadastrocliente/src/main/java/com/cadastrocliente/controllers/ClienteController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
