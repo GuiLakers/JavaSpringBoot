c36397e56fd350f20c1b15aab2d548abeb4070ca
GuiLakers08 <guifun080808@gmail.com>
2021-03-21 20:35:00 +0000

    Up

--- cadastrocliente/src/main/java/com/cadastrocliente/controllers/CidadeController.java
+++ cadastrocliente/src/main/java/com/cadastrocliente/controllers/CidadeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cidades")
+@RequestMapping("/cidade")
 public class CidadeController {
 
     @Autowired
